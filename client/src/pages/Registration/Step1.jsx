0278e5b16c8b7df78ca0480d453dbb50a41782e7
yt29501240201877-cloud <yt29501240201877@depi.eui.edu.eg>
2026-05-11 13:36:37 +0300

    Step 1

--- client/src/pages/Registration/Step1.jsx
+++ client/src/pages/Registration/Step1.jsx
@@ -1,13 +1,31 @@
 import Style from './Step1.module.css'
 import icon from '../../assets/Icon.png'
+import avatar from '../../assets/avatar.png'
 
 export default function Step1() {
   return (
-    <div className={`d-flex ${Style.main}`}>
-          <div className={Style.leftside}>
-            <div className={Style.brand}>
+    <div className={`d-flex align-items-center ${Style.main}`}>
+          <div className={`d-flex flex-column justify-content-between ${Style.leftside}`}>
+            <div className="container">
+              <div className={Style.brand}>
               <img src={icon} alt="BloodLink icon" />
                 BloodLink
+              </div>
+              <div className={Style.leftContainer}>
+                <h1>Create New Donor Account</h1>
+                <p>Join a community of lifesavers. Your contribution helps ensure a steady supply of blood for patients in need within the Central Region.</p>
+              </div>
+            </div>
+            <div className={Style.comment}>
+              <blockquote>
+                "Donating blood is a simple act that makes a massive impact. BloodLink made my first donation experience incredibly easy."
+              </blockquote>
+              <div className={`d-flex align-items-center ${Style.commentuser}`}>
+                <img src={avatar} />
+                <div className="username">
+                  Sarah M., Regular Donor
+                </div>
+              </div>
             </div>
           </div>
     </div>
