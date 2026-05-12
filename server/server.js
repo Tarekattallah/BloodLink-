9e06398340a6f936e637a98fe5217840e49ae60d
yt29501240201877-cloud <yt29501240201877@depi.eui.edu.eg>
2026-05-12 19:41:21 +0300

    Login full

--- server/server.js
+++ server/server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-
+const cors = require('cors');
 const connectDB = require('./config/db.js');
 
 const { PORT } = require('./config/env.js')
 
 const app = express();
 
+
 connectDB();
 
 app.use(express.json());
 
-
+app.use(cors());
 //Routes 
 const authRoutes = require('./routes/authRoutes.js')
 app.use('/api/auth', authRoutes);
