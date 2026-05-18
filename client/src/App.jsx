5bad1d6c16d430bb25313aa382dcaaf6c072710c
yt29501240201877-cloud <yt29501240201877@depi.eui.edu.eg>
2026-05-18 13:56:05 +0300

    Mod

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import AdminDashboard from './pages/AdminDashboard/AdminDashboard'
+import ImpactHub from './pages/community/ImpactHub'
 import Step1 from './pages/Registration/Step1'
 import './App.css'
 import Login from './pages/auth/Login'
-
-import ImpactHub from './pages/community/ImpactHub'
 
 const router = createBrowserRouter([
   { path: '/', element: <Login /> },
   { path: '/registration', element: <Step1 /> },
   { path: '/Admin', element: <AdminDashboard />,
     children: [
-      {path: "/impact", element: <ImpactHub />}
+      {path: "impact", element: <ImpactHub />}
     ]
   },
 ])
